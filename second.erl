0939322111208a4f01ef8c2bc1f1974bbc6f2230
Ruben Amortegui <ruben.amortegui@gmail.com>
2017-02-22 22:15:20 -0700

    Add right angle triangle perimeter and area functions.

--- second.erl
+++ second.erl
@@ -1,5 +1,11 @@
 -module(second).
--export([hipo/2]).
+-export([hipo/2,area/2,perim/2]).
 
 hipo(B,H) ->
   math:sqrt(first:square(B)+first:square(H)).
+
+area(B,H) ->
+  first:mult(B,H)/2.
+
+perim(B,H) ->
+  hipo(B,H)+B+H.
